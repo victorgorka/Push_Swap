2e52a1508ab2ff9e9a6d3b99e9a87459449e3910
Victor gorka De prada arnedo <vde-prad@student.42malaga.com>
2022-11-23 18:56:30 +0100

    main

--- push_swap.c
+++ push_swap.c
@@ -6,11 +6,23 @@
 /*   By: vde-prad <vde-prad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 12:54:20 by vde-prad          #+#    #+#             */
-/*   Updated: 2022/11/23 13:59:04 by vde-prad         ###   ########.fr       */
+/*   Updated: 2022/11/23 18:56:17 by vde-prad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	ft_sort3(char	**argv)
+{
+	int	stack[3];
+
+
+}
+
 int	main(int argc, char	**argv)
 {
+	if (argc <= 4)
+		ft_sort3(argv);
 	
 }
+//if 1st number is highest -->ra
+//if 2nd numbeer is highest -->rra
+//if  1st  number > 2nd number --> sa
