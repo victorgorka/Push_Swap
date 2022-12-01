e7b7ce5c7025c8d955cbdc7f994172b579a2fa65
Victor gorka De prada arnedo <vde-prad@student.42malaga.com>
2022-12-01 17:29:08 +0100

    add node

--- push_swap.c
+++ push_swap.c
@@ -6,23 +6,42 @@
 /*   By: vde-prad <vde-prad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 12:54:20 by vde-prad          #+#    #+#             */
-/*   Updated: 2022/11/23 18:56:17 by vde-prad         ###   ########.fr       */
+/*   Updated: 2022/12/01 17:27:52 by vde-prad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "push_swap.h"
 
-void	ft_sort3(char	**argv)
+
+t_stack		*ft_newnode()
 {
-	int	stack[3];
+	t_stack	*node;
 
+	node = malloc(sizeof(t_stack));
+	node->value = 0;
+	node->index = 0;
+	node->pos = 0;
+	node->target_pos = 0;
+	node->cost_a = 0;
+	node->cost_b = 0;
+	node->next = 0;
+	return (node);
+}
 
+ft_fillstack(t_stack **a, t_stack new)
+{
+	
 }
 
 int	main(int argc, char	**argv)
 {
-	if (argc <= 4)
-		ft_sort3(argv);
-	
+	t_stack	*a;
+
+	a = ft_newnode();
+	while (argc-- > 1)
+		ft_fillstack(&a, &ft_newnode());
 }
 //if 1st number is highest -->ra
 //if 2nd numbeer is highest -->rra
 //if  1st  number > 2nd number --> sa
+//
+// TODO pop y push
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: vde-prad <vde-prad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 12:25:36 by vde-prad          #+#    #+#             */
-/*   Updated: 2022/11/23 13:27:01 by vde-prad         ###   ########.fr       */
+/*   Updated: 2022/12/01 14:37:20 by vde-prad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define PUSH_SWAP_H
 
 //*********************** STANDARD LIBRARIES *********************************
-
+#include <stdlib.h>
 //************************** DEFINITIONS *************************************
 typedef struct s_stack
 {
@@ -24,8 +24,7 @@ typedef struct s_stack
 	int				target_pos;
 	int				cost_a;
 	int				cost_b;
-	struct s_stack	next;
+	struct s_stack	*next;
 }	t_stack;
 //*************************** FUNCTIONS **************************************
-
 #endif
