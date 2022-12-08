466e79f2b109bbd30442f2738d73a463a78a8bab
Victor gorka De prada arnedo <vde-prad@student.42malaga.com>
2022-12-08 21:10:22 +0100

    vamos

--- push_swap.c
+++ push_swap.c
@@ -6,18 +6,18 @@
 /*   By: vde-prad <vde-prad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 12:54:20 by vde-prad          #+#    #+#             */
-/*   Updated: 2022/12/01 17:27:52 by vde-prad         ###   ########.fr       */
+/*   Updated: 2022/12/08 21:02:58 by vde-prad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
 
 
-t_stack		*ft_newnode()
+t_stack	*ft_newnode(int value)
 {
 	t_stack	*node;
 
 	node = malloc(sizeof(t_stack));
-	node->value = 0;
+	node->value = value;
 	node->index = 0;
 	node->pos = 0;
 	node->target_pos = 0;
@@ -27,18 +27,37 @@ t_stack		*ft_newnode()
 	return (node);
 }
 
-ft_fillstack(t_stack **a, t_stack new)
+void ft_fillstack(t_stack **a, int argc, char **argv)
 {
-	
+	t_stack	*node;
+	int		i;
+
+	i = 1;
+	*a = ft_newnode(atoi(argv[i++]));
+	node = *a;
+	while (i < argc)
+	{
+		node->next = ft_newnode(atoi(argv[i++]));
+		node = node->next;
+	}
 }
 
 int	main(int argc, char	**argv)
 {
-	t_stack	*a;
+	t_stack	*a;	
 
-	a = ft_newnode();
-	while (argc-- > 1)
-		ft_fillstack(&a, &ft_newnode());
+	a = NULL;
+	if (argc > 1)
+		ft_fillstack(&a, argc, argv);
+	else
+		return (0);
+
+	t_stack *n = a;
+	while (n)
+	{
+		printf("value: %d\n", n->value);
+		n = n->next;
+	}
 }
 //if 1st number is highest -->ra
 //if 2nd numbeer is highest -->rra
