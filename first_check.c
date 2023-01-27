f15e695c892337ec2094d48074831ce6a606d685
Victor gorka De prada arnedo <vde-prad@student.42malaga.com>
2023-01-27 15:20:00 +0100

    filling args vartiable fixed

--- first_check.c
+++ first_check.c
@@ -6,7 +6,7 @@
 /*   By: vde-prad <vde-prad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 16:20:14 by vde-prad          #+#    #+#             */
-/*   Updated: 2023/01/26 17:23:09 by vde-prad         ###   ########.fr       */
+/*   Updated: 2023/01/27 14:01:19 by vde-prad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
@@ -14,11 +14,25 @@
 unsigned int ft_check_args(int argc, char **argv)
 {
 	char	**args;
+	int		i;
 
+	i = 0;
 	if (argc == 2)
 		args = ft_split(argv[1], ' ');
 	else
-		args = &argv[1];
+	{
+		args = malloc(sizeof(char **));
+		*args = malloc((argc - 1) * sizeof(char	*));
+		while (i < (argc - 1))
+		{
+			args[i] = ft_strdup(argv[i + 1]);
+			i++;
+		}
+		args[argc - 1] = NULL;
+	}
+	i = 0;
+	while (i < 4)
+		ft_printf("%s\n", args[i++]);
 	return (0);
 }
 
