cf55b2da988fc0979442b64924e196d1531bb972
Victor gorka De prada arnedo <vde-prad@student.42malaga.com>
2023-02-02 13:24:21 +0100

    ft_check_duplicate finished; error management finished

--- first_check.c
+++ first_check.c
@@ -6,7 +6,7 @@
 /*   By: vde-prad <vde-prad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 16:20:14 by vde-prad          #+#    #+#             */
-/*   Updated: 2023/02/01 18:57:10 by vde-prad         ###   ########.fr       */
+/*   Updated: 2023/02/02 13:23:21 by vde-prad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
@@ -21,10 +21,6 @@ unsigned int ft_check_args(int argc, char **argv, t_data *data)
 		data->args = ft_split(argv[1], ' ');
 	else
 		data->args = &argv[1];
-	// printf("%s\n", data->args[4]);
-	//free(data->args[0]);
-//	if (!data->args)
-//		return (0);
 	if (!ft_check_str(data) || !ft_check_int(data))
 		ft_error();
 	return (1);
@@ -32,21 +28,43 @@ unsigned int ft_check_args(int argc, char **argv, t_data *data)
 
 int	ft_check_int(t_data *data)
 {
-	int	i;
+	unsigned int	i;
 
 	i = 0;
 	while (data->args[i])
 		i++;
+	data->iargs_len = i - 1;
 	data->iargs = malloc((i + 1) * sizeof(int));
 	i = -1;
 	while (data->args[++i])
 	{
-		data->iargs[i] = ft_atoll(data->args[i]);
-		if (data->iargs[i] > INT_MAX || data->iargs[i] < INT_MIN)
+		if (ft_atoll(data->args[i]) > INT_MAX 
+			|| ft_atoll(data->args[i]) < INT_MIN)
 			return (0);
+		data->iargs[i] = ft_atoi(data->args[i]);
 	}
-	free(data->iargs);
-	// data->iargs[i] = NULL;
+	if (!ft_check_duplicate(data))
+		return (0);
+	return (1);
+}
+
+int	ft_check_duplicate(t_data *data)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	while (i <= data->iargs_len)
+	{
+		j = 0;
+		while (j <= data->iargs_len)
+		{
+			if (i != j && data->iargs[i] == data->iargs[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
 	return (1);
 }
 
@@ -88,24 +106,20 @@ int main(int argc, char	**argv)
 {
 	t_data data;
 	int i= 0;
-//	(void) argc;
-//	data = (t_data *)malloc(sizeof(t_data));
-//	data->args = ft_split(argv[1], ' ');
 
 	ft_check_args(argc, argv, &data);
-	// puts(data.args[0]);
-//	printf("%s\n", data->args[0]);
+	puts(data.args[0]);
+	// printf("%lld\n", data.iargs[0]);
 	if (argc == 2)
 	{
 		 while (data.args[i])
 		 {
 			 if (data.args[i])
 				free(data.args[i++]);
-//			free(data.args[i]);
 		}
 		free(data.args);	
 	}
-//	free(data);
+	free(data.iargs);
 	atexit(check_leaks);
 }
 //CASOS DE ERROR
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: vde-prad <vde-prad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 12:25:36 by vde-prad          #+#    #+#             */
-/*   Updated: 2023/02/01 18:54:04 by vde-prad         ###   ########.fr       */
+/*   Updated: 2023/02/02 13:05:40 by vde-prad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,16 +32,18 @@ typedef struct s_stack
 
 typedef struct s_data
 {
-	t_stack		*a;
-	t_stack		*b;
-	char		**args;
-	long long	*iargs;
-	int			argc;
+	t_stack			*a;
+	t_stack			*b;
+	char			**args;
+	int				*iargs;
+	int				argc;
+	unsigned int	iargs_len;
 }	t_data;
 //*************************** FUNCTIONS **************************************
 unsigned int	ft_check_args(int argc, char **argv, t_data *data);
 int				ft_check_str(t_data *data);
 int				ft_check_int(t_data *data);
+int				ft_check_duplicate(t_data *data);
 long long		ft_atoll(const char *str);
 void			ft_error();
 #endif
