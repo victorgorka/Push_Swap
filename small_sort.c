7b9eaeb099c74f50957ef970ea9e09a820f0f378
Victor gorka De prada arnedo <vde-prad@student.42malaga.com>
2023-02-20 17:30:28 +0100

    and fixed

--- small_sort.c
+++ small_sort.c
@@ -6,7 +6,7 @@
 /*   By: vde-prad <vde-prad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 17:04:18 by vde-prad          #+#    #+#             */
-/*   Updated: 2023/02/18 21:16:29 by vde-prad         ###   ########.fr       */
+/*   Updated: 2023/02/20 17:27:00 by vde-prad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	ft_small_sort(t_data *data)
 			&& data->a->value > data->a->next->next->value)
 			ft_ra(data);
 		else if (data->a->next->value > data->a->value
-			& data->a->next->value > data->a->next->next->value)
+			&& data->a->next->value > data->a->next->next->value)
 			ft_rra(data);
 		else if (data->a->value > data->a->next->value)
 			ft_sa(data);
